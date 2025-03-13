eb14a0b201c8c2f5e319b0c9d2784d59f8141b91
FaeWulfs <ngolamaz3@gmail.com>
2025-03-13 15:02:03 +0700

    + Add: forge support.

---
+++ forge/src/main/java/xyz/faewulf/lib/Forge.java
@@ -0,0 +1,27 @@
+package xyz.faewulf.lib;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import xyz.faewulf.lib.util.config.infoScreen.ModInfoScreen;
+
+@Mod(Constants.MOD_ID)
+public class Forge {
+
+    public Forge() {
+        CommonClass.init();
+        Constants.LOG.info("Init done");
+    }
+
+
+    @Mod.EventBusSubscriber(value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            //config
+            MinecraftForge.registerConfigScreen((client, parent) -> ModInfoScreen.getScreen(parent, Constants.MOD_ID));
+        }
+    }
+}
