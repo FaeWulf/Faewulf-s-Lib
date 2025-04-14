140b1662b67efa373de77fccf94ac0ab4888b782
FaeWulfs <ngolamaz3@gmail.com>
2025-04-14 13:53:50 +0700

    + Add: Example for config system.

--- common/src/main/java/xyz/faewulf/lib/util/config/ModConfigs.java
+++ common/src/main/java/xyz/faewulf/lib/util/config/ModConfigs.java
@@ -6,25 +6,26 @@ import xyz.faewulf.lib.Constants;
 @ModConfig(mod_id = Constants.MOD_ID)
 public class ModConfigs {
 
-    @Entry(category = "Client", name = "Enable mod", require_restart = false)
-    public static boolean __enable_mod = true;
+    @Entry(category = "Client", name = "Boolean", require_restart = false)
+    public static boolean boolean_test = true;
 
-    @Entry(category = "Client", name = "Enable Cloud Sync", require_restart = false)
-    public static boolean _enable_cloud_sync = true;
+    @Entry(category = "Client", name = "Enum type", require_restart = false)
+    public static boolean enum_test = true;
 
-    @Entry(category = "Client", name = "Enable dynamic light", require_restart = false)
-    public static boolean enable_dynamiclight_compat = true;
+    @Entry(category = "Client", name = "Int value", require_restart = false, group = "Number option")
+    public static int int_test = 10;
 
-    @Entry(category = "Client", name = "Hide backpack if invisible", require_restart = false)
-    public static boolean hide_if_invisible = true;
+    @Entry(category = "Client", name = "Int Slider", require_restart = false, group = "Number option")
+    @SliderEntry(min = 0, max = 100, step = 2)
+    public static int int_slider = 50;
 
-    @Entry(category = "Server", name = "Hide items component data", require_restart = false)
-    public static boolean hide_items_component_data = false;
+    @Entry(category = "Server", name = "Boolean with caution", require_restart = true)
+    public static boolean boolean_with_caution = false;
 
-    @Entry(category = "style", name = "Backpack Model Type", require_restart = false, hidden = true)
-    public static String backpack = "default";
+    @Entry(category = "style", name = "Hidden value 1", require_restart = false, hidden = true)
+    public static String hidden_value = "default";
 
-    @Entry(category = "style", name = "Variant", require_restart = false, hidden = true)
-    public static String variant = "default";
+    @Entry(category = "style", name = "Hidden value 2", require_restart = false, hidden = true)
+    public static String hidden_value_2 = "default";
 
 }
